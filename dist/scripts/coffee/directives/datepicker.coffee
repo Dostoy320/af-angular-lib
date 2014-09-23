1d0c3d285bf0385d79f2d5dbd9691733d1fd82eb
Nate Alberg <alberg.nate@gmail.com>
2014-09-23 15:15:45 -0500

    added date picker

---
+++ dist/scripts/coffee/directives/datepicker.coffee
@@ -0,0 +1,36 @@
+myApp = angular.module('af.datePicker', [])
+myApp.directive 'datePicker', ($parse)->
+  return {
+    require: 'ngModel'
+    restrict:'A'
+    replace:true
+    transclude:false
+    compile: (scope, element, attrs) ->
+      # create ui input
+      modelAccessor = $parse(attrs.ngModel)
+      html = '<input type="text" id="' + attrs.id + '"></input>'
+      newElm = $(html)
+      element.replace(newElm)
+
+      return (scope, element, attrs, controller) ->
+
+        onchange = () ->
+          date = new Date(element.datepicker('getDate'))
+          scope.$apply (scope) ->
+            modelAccessor.assign(scope, date)
+
+        # init date picker
+        element.datepicker({
+          inline:true
+          onClose:onchange
+          onSelect:onchange
+        })
+
+        # watch for changes
+        scope.$watch modelAccessor, (val) ->
+          date = new Date(val)
+          element.datepicker('setDate', date)
+
+
+
+  }
---
+++ dist/scripts/js/directives/datepicker.js
@@ -0,0 +1,42 @@
+(function() {
+  var myApp;
+
+  myApp = angular.module('af.datePicker', []);
+
+  myApp.directive('datePicker', function($parse) {
+    return {
+      require: 'ngModel',
+      restrict: 'A',
+      replace: true,
+      transclude: false,
+      compile: function(scope, element, attrs) {
+        var html, modelAccessor, newElm;
+        modelAccessor = $parse(attrs.ngModel);
+        html = '<input type="text" id="' + attrs.id + '"></input>';
+        newElm = $(html);
+        element.replace(newElm);
+        return function(scope, element, attrs, controller) {
+          var onchange;
+          onchange = function() {
+            var date;
+            date = new Date(element.datepicker('getDate'));
+            return scope.$apply(function(scope) {
+              return modelAccessor.assign(scope, date);
+            });
+          };
+          element.datepicker({
+            inline: true,
+            onClose: onchange,
+            onSelect: onchange
+          });
+          return scope.$watch(modelAccessor, function(val) {
+            var date;
+            date = new Date(val);
+            return element.datepicker('setDate', date);
+          });
+        };
+      }
+    };
+  });
+
+}).call(this);
