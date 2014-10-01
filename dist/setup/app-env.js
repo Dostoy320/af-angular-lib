4ebe7c99a231f70411f249581c0dbdf8447eea29
Nate Alberg <alberg.nate@gmail.com>
2014-10-01 15:48:16 -0500

    updated app-env

--- dist/setup/app-env.js
+++ dist/setup/app-env.js
@@ -2,23 +2,82 @@
 //  BASIC APP SETUP NEEDS
 //
 window.appEnv = {
+
+  // tenant/index for development
+  dev:{
+    localhost:{
+      tenant:'td',
+      index:'alpha2'
+    },
+    alpha:{
+      tenant:'waddell',
+      index:'alpha'
+    },
+    alpha2:{
+      tenant:'td',
+      index:'alpha2'
+    }
+  },
+
+
+  //
+  //  SUB DOMAIN
+  //
   getSubDomain : function(){
     return (window.location.host).split('.').shift().toLowerCase();
   },
+  // strips -dev off off sub domains
+  getCleanSubDomain:function(){
+    var subDomain = appEnv.getSubDomain();
+    if(subDomain.indexOf('-dev') > -1) return subDomain.split('-').shift();
+    return subDomain;
+  },
+
+
+  //
+  //  ENVIRONMENT
+  //
   getEnv : function(){
     var subDomain = appEnv.getSubDomain();
-    if(subDomain === 'localhost') return 'dev';
-    if(subDomain === 'dev') return 'dev';
-    if(subDomain.indexOf('alpha') === 0) return 'dev';
-    if(subDomain.indexOf('-dev') > -1) return 'dev';
+    if(subDomain === 'localhost')         return 'dev';
+    if(subDomain === 'dev')               return 'dev';
+    if(subDomain.indexOf('alpha') === 0)  return 'dev';
+    if(subDomain.indexOf('-dev') > -1)    return 'dev';
     return 'prod';
   },
+  // is development quickie
+  isDev : function(){ return appEnv.getEnv() === 'dev' },
+
+
+  //
+  //  TENANT
+  //
   getTenant : function() {
-    var subDomain = appEnv.getSubDomain();
-    if(appEnv.getEnv() === 'dev') return 'td';
+    var subDomain = appEnv.getCleanSubDomain();
+    // check for special cases
     switch (subDomain) {
-      case 'tdai': return 'td';
+      case 'dev':
+      case 'localhost': return appEnv.dev.localhost.tenant;
+      case 'alpha':     return appEnv.dev.alpha.tenant;
+      case 'alpha2':    return appEnv.dev.alpha2.tenant;
+      case 'tdai':      return 'td';
     }
     return subDomain;
+  },
+
+  getTenantIndex : function() {
+    var index = appEnv.getTenant();
+    var subDomain = appEnv.getCleanSubDomain();
+    // check for special cases
+    switch(subDomain){
+      case 'dev':
+      case 'localhost': return appEnv.dev.localhost.index;
+      case 'alpha':     return appEnv.dev.alpha.index;
+      case 'alpha2':    return appEnv.dev.alpha2.index;
+      case 'tdai':      return 'td';
+      case 'waddell':   return 'wr';
+    }
+    return index;
   }
+
 }
