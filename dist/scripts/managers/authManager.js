956a390b08e03aea693ad4e01d5c2eb9965e9abd
Nate Alberg <alberg.nate@gmail.com>
2014-12-01 17:25:33 -0600

    lotsa changes.

--- dist/scripts/managers/authManager.js
+++ dist/scripts/managers/authManager.js
@@ -102,9 +102,7 @@
       },
 
       isAdmin: function() { return auth.hasAnyRole([auth.Role_Admin, auth.Role_RoadmapUserAdmin, auth.Role_RoadmapContentAdmin]); },
-      isCoach: function() {
-        return auth.hasAnyRole([auth.Role_AccessKeyManager]);
-      }
+      isCoach: function() { return auth.hasAnyRole([auth.Role_AccessKeyManager]);  }
 
 
     };
