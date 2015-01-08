5021e182f5b472d0360d79b5c9a1ee17d57fa039
Nate Alberg <alberg.nate@gmail.com>
2015-01-08 15:58:46 -0600

    loader re-worked.

--- dist/scripts/system/loader.js
+++ dist/scripts/system/loader.js
@@ -16,8 +16,8 @@ angular.module('af.loader', ['af.event'])
 
       // util / quickies
       isLoading:function(){ return isRunning; },
-      saving: function() { srv.start('Saving...');    },
-      loading: function() { srv.start('Loading...');  },
+      saving: function() { srv.start('Saving');    },
+      loading: function() { srv.start('Loading');  },
       bar: function() { srv.start({bar:true, mask:false});  },
       mask: function() { srv.start({bar:false, mask:true});  }
     };
@@ -30,26 +30,40 @@ angular.module('af.loader', ['af.event'])
       scope: {},
       template: '<div class="ng-cloak">' +
                   '<div id="app-loader-bar" ng-cloak ng-show="loaderBar" class="ng-cloak progress progress-striped active">' +
-                  //'<div id="app-loader-bar" ng-cloak class="ng-cloak progress progress-striped active">' +
                     '<div class="progress-bar" style="width:100%"></div>' +
                   '</div>' +
                   '<div id="app-loader-mask" ng-show="loadMask">' +
-                  //'<div id="app-loader-mask">' +
                     '<div class="loader-mask"></div>' +
-                    '<div class="loader-text">' +
-                      '<i class="icon-spinner icon-spin icon-3x"></i> &nbsp;' +
-                      '<span ng-show="loaderText" ng-bind="loaderText"></span>' +
-                      '<span ng-show="loaderText" ng-bind="dots"></span>' +
+                    '<div class="loader-text" ng-show="loaderText">' +
+                      '<div class="loader-gear"><span fa-icon="gear" class="fa-spin fa-2x" style="line-height:20px; vertical-align: middle;"></span></div>' +
+                      '<span class="icon-spinner icon-spin icon-3x"></span> &nbsp;' +
+                      '<span ng-bind="loaderText"></span><span>...</span>' +
                     '</div>' +
                   '</div>' +
                 '</div>',
       link: function(scope, element, attrs) {
-        scope.dots = '.';
+        scope.dots = 3;
         scope.loaderBar = null;
         scope.loadMask = null;
         scope.loaderText = null;
 
         var timer = null;
+        var addDots = function(){
+          scope.dots += 1;
+          if(scope.dots == 4) scope.dots = 0;
+        }
+        var clearTick = function(){
+          if(timer) $interval.cancel(timer);
+        }
+        var startTick = function(){
+          clearTick();
+          if(!scope.loaderText) return;
+          scope.loaderText.replace('\.','');
+          if(scope.loaderText.substr(scope.loaderText.length - 3) == '...')
+            scope.loaderText = scope.loaderText.substring(0, scope.loaderText.length - 3);
+          addDots();
+          timer = $interval(addDots, 600);
+        }
 
         scope.start = function(options) {
           if(_.isString(options)){
@@ -61,15 +75,19 @@ angular.module('af.loader', ['af.event'])
             scope.loadMask = options.hasOwnProperty('mask') ? options.mask : scope.loaderText; // show mask if text
             scope.loaderBar = options.hasOwnProperty('bar') ? options.bar : true
           }
-          //timer = $interval(addDots, 750)
+          startTick();
         };
         scope.stop = function() {
           scope.loaderBar = scope.loaderText = scope.loadMask = null;
+          clearTick();
         };
         scope.$on($event.EVENT_loaderStart, function(event, txt) {
           scope.start(txt);
         });
         scope.$on($event.EVENT_loaderStop, scope.stop);
+
+        // kill any timer on destroy
+        element.on('$destroy', clearTick);
       }
     };
   })
