4fa7a5f83683a483d658282a63d724e3be46a521
Nate Alberg <alberg.nate@gmail.com>
2015-02-11 15:55:42 -0600

    roadmaps loading

--- dist/scripts/directives/bs-icon.js
+++ dist/scripts/directives/bs-icon.js
@@ -17,13 +17,13 @@
         switch((''+attrs.faIcon).toLowerCase()){
           case 'roadmap': attrs.faIcon = 'road'; break; //'map-marker';
           case 'assessment': attrs.faIcon = 'check-circle-o'; break;
-          case 'quickContent': attrs.faIcon = 'file-text-o'; break;
-          case 'file':
-          case 'export':
-            attrs.faIcon = 'file'; break;
-          case 'pdf':  attrs.faIcon = 'file-pdf-o'; break;
-          case 'rtf':  attrs.faIcon = 'file-word-o'; break;
-          case 'csv':  attrs.faIcon = 'file-excel-o'; break;
+          case 'quickcontent':
+          case 'quick content':
+            attrs.faIcon = 'file-text-o'; break;
+          case 'export':  attrs.faIcon = 'file'; break;
+          case 'pdf':     attrs.faIcon = 'file-pdf-o'; break;
+          case 'rtf':     attrs.faIcon = 'file-word-o'; break;
+          case 'csv':     attrs.faIcon = 'file-excel-o'; break;
         }
         angular.element(elm).addClass('ng-show-inline fa fa-' + attrs.faIcon);
       }
