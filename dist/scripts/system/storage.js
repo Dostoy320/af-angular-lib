f2dd52a378d7fcdc0009b171e81b8a2ba73596d1
Nate Alberg <alberg.nate@gmail.com>
2015-09-24 14:38:56 -0500

    lotsa simplifications

--- dist/scripts/system/storage.js
+++ dist/scripts/system/storage.js
@@ -6,100 +6,93 @@
   //
 angular.module('af.storage', [])
 
-  .constant('$STORAGE_CONFIG', { namespace:'myApp' } )
+  .constant('$STORAGE_CONFIG', {persistent_prefix:'myApp'} )
 
-  .service('$storage', function($STORAGE_CONFIG, $window, $log) {
-
-    // transfers sessionStorage from one tab to another
-    var sessionStorage_transfer = function(event) {
-      if (!event) { event = window.event; } // ie
-      $log.info('storage event', event);
-      if (event.key == 'getSessionStorage') {
-        // Some tab asked for the sessionStorage -> send it
-        localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
-        localStorage.removeItem('sessionStorage');
-      } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
-        var data = JSON.parse(event.newValue);
-        for (var key in data) {
-          sessionStorage.setItem(key, data[key]);
-        }
-      }
-    };
-
-    // Ask other tabs for session storage
-    if (!sessionStorage.length) {
-      localStorage.setItem('getSessionStorage', 'foobar');
-      localStorage.removeItem('getSessionStorage');
-    };
-    if($window.addEventListener) {
-      $window.addEventListener("storage", sessionStorage_transfer, false);
-    } else {
-      $window.attachEvent("onstorage", sessionStorage_transfer);
-    };
-
-
-
-    var optionsCheck = function(options){
-      if(_.isNumber(options)) return { expires:options };
-      return options;
-    };
-    var getAllData = function(values){
-      var data = {};
-      _.each(values, function(value, key){
-        if(storage.isAppData(key)) data[key] = angular.copy(value);
-      });
-      return data;
-    };
-
-
+  .service('$storage', function($STORAGE_CONFIG, $log) {
 
     var tempData = {}; // cleared if page refreshed..
-    var prefix = $STORAGE_CONFIG.namespace;
+    var prefix = $STORAGE_CONFIG.persistent_prefix;
+
+
+    //// transfers sessionStorage from one tab to another
+    //var sessionStorage_transfer = function(event) {
+    //  if(!event) { event = window.event; } // ie suq
+    //  if(!event.newValue) return;          // do nothing if no value to work with
+    //  if (event.key == 'getSessionStorage') {
+    //    // another tab asked for the sessionStorage -> send it
+    //    localStorage.setItem('sessionStorage', JSON.stringify(sessionStorage));
+    //    // the other tab should now have it, so we're done with it.
+    //    localStorage.removeItem('sessionStorage'); // <- could do short timeout as well.
+    //  } else if (event.key == 'sessionStorage' && !sessionStorage.length) {
+    //    // another tab sent data <- get it
+    //    var data = JSON.parse(event.newValue);
+    //    for (var key in data) {
+    //      sessionStorage.setItem(key, data[key]);
+    //    }
+    //  }
+    //};
+    //
+    //// listen for changes to localStorage
+    //if(window.addEventListener) {
+    //  window.addEventListener("storage", sessionStorage_transfer, false);
+    //} else {
+    //  window.attachEvent("onstorage", sessionStorage_transfer);
+    //};
+    //
+    //
+    //// Ask other tabs for session storage (this is ONLY to trigger event)
+    //if (!sessionStorage.length) {
+    //  localStorage.setItem('getSessionStorage', 'foobar');
+    //  localStorage.removeItem('getSessionStorage', 'foobar');
+    //};
 
     var storage = {
+      logCachedData:true, // dev
 
       // amplify wrapper
-      store:function(key, value, options){
-        options = optionsCheck(options);
+      amplify:function(key, value, options){
+        if(_.isNumber(options)) options = { expires:options };
         return amplify.store(key, value, options);
       },
 
 
-      // TEMP cleared if page refreshed...
+      //
+      // STORE
+      //
+      // store till cleared... (amplify alias)
+      local:function(key, value, options){
+        return storage.amplify(key, value, options);
+      },
+
+      // store till cleared or next login...
+      session:function(key, value, options){
+        // get or set
+        if(arguments.length > 0) return storage.amplify(prefix+'_'+key, value, options);
+        // get all data
+        var appData = {};
+        _.each(amplify.store(), function(value, key){
+          if(storage.isAppData(key)) appData[key] = angular.copy(value);
+        });
+        return appData;
+      },
+
+      // store till cleared, next login, or page refresh...
       temp:function(key, value){
         if(arguments.length == 0) return tempData;
-        if(arguments.length == 1) return tempData[key];
+        if(arguments.length == 1) {
+          if(storage.logCachedData) $log.info('TEMP CACHE:' + key);
+          return tempData[key];
+        }
         tempData[key] = angular.copy(value);
       },
 
 
-      // store till window closed
-      session:function(key, value, options){
-        options = optionsCheck(options);
-        if(!amplify.store.types.sessionStorage){
-          // get / set data
-          if(arguments.length > 0) return amplify.store.sessionStorage(prefix+key, value, options);
-          return getAllData(amplify.store.sessionStorage());
-        } else {
-          // fallback
-          if(arguments.length > 0) return amplify.store(key, value, options);
-          return getAllData(amplify.store());
-        }
-      },
 
-      // store till expires
-      local:function(key, value, options){
-        options = optionsCheck(options);
-        return storage.store(key, value, options);
-      },
-
-
-
-      
       //
       // EMPTY
       //
       clear: function(key) {
+        // clear one thing
         if(key){
           delete tempData[key];
           return amplify.store(prefix+'_'+key, null);
@@ -111,16 +104,14 @@ angular.module('af.storage', [])
         });
       },
 
-      // clear ALL data
+      // clear everything
       nuke:function(){
         _.keys(amplify.store(), function(key){
           amplify.store(key, null);
         });
       },
 
-      isAppData:function(key){
-        return (''+key).indexOf(prefix+'_') === 0;
-      }
+      isAppData:function(key){ return key.indexOf(prefix+'_') === 0; }
 
     };
 
